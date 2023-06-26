23eb3f1980a4353f927eaae859122009a9e253fa
Victor Carnavali <133589453+VictorCarnavali@users.noreply.github.com>
2023-06-26 14:50:58 -0300

    Update main.js

--- p09-to do/main.js
+++ p09-to do/main.js
@@ -5,12 +5,12 @@ let myList = [];
 
 function addNewTask(){
     myList.push(input.value);
-    
+
     showTasks()
 }
 
 function showTasks(){
-
+     let newLi = ;
 }
 
 button.addEventListener('click', addNewTask);
